2513094ac0410ed165ef4a931e8544ecf2e83919
Matheus Rocha <matheusgarragarra@gmail.com>
2020-06-25 10:28:31 -0300

    Funções Base das opções

---
+++ Linguagem.js
@@ -0,0 +1,15 @@
+function Antitese() {
+    window.alert('Antitese')
+}
+function Paradoxo() {
+    window.alert('Paradoxo')
+}
+function Ironia() {
+    window.alert('Ironia')
+}
+function Sinestesia() {
+    window.alert('Sinestesia')
+}
+function Ambiguidade() {
+    window.alert('Ambiguidade')
+}
